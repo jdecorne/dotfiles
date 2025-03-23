b7cf5f30cb50531fdd32673f97ae29142c90c260
Jonathan Decorne <jdecorne@jdecorn-ltmhv44.internal.salesforce.com>
2025-03-23 18:30:23 +0100

    Updated settings

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,5 +1,9 @@
-# spaceship theme
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="spaceship"
+plugins=(git dirhistory)
+source $ZSH/oh-my-zsh.sh
 
+# spaceship theme
 SPACESHIP_PROMPT_SYMBOL='$'
 SPACESHIP_PROMPT_TRUNC=0
-
+SPACESHIP_GIT_SHOW=false
